76819b88ebaba98ffca730932fedc33110ea7d82
init0-lux <init0.lux@protonmail.com>
2025-03-23 07:27:09 +0530

    Added middleware;

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import cookieParser from 'cookie-parser';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subsRouter from './routes/subscription.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // to specify what routes the app must use
 //
@@ -17,6 +23,8 @@ const app = express();
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subsRouter);
+
+app.use(errorMiddleware);
 
 // route and callback function
 app.get( '/', ( req, res ) => {
